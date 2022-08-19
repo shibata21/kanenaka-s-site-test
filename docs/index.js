3f0e0b076b2be022d032c8f6ac498363bce52390
popenti <popenti21@gmail.com>
2022-08-19 10:20:05 +0900

    c

--- docs/index.js
+++ docs/index.js
@@ -1,49 +1,56 @@
 $(document).ready(function () {
-    $('#form').submit(function (event) {
-      var formData = $('#form').serialize();
-      $.ajax({
-        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSf-Vgq0jL1BWyXUMUEH02DYWNri1dI1_jKNgeyWFCP7HKFE3Q/formResponse",
-        data: formData,
-        type: "POST",
-        dataType: "xml",
-        statusCode: {
-          0: function () {
-        console.log("aaaa");
-            $("#end-message").slideDown();
-            $("#form").fadeOut();
-            //window.location.href = "thanks.html";
-          },
-          200: function () {
-        console.log("cccc");
-            $("#false-message").slideDown();
-          }
+  //お問い合わせフォーム処理
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/5/d/e/1FAIpQLSd78_QOCOJVhgG4u4ODoIDr2XH1r71JeADlDKWCBzBXoTMIBA/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $("#end-message").slideDown();
+          $("#form").fadeOut();
+        },
+        200: function () {
+          $("#false-message").slideDown();
         }
-      });
-      event.preventDefault();
+      }
     });
-// Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+    event.preventDefault();
   });
+
+  //topへボタン
+  let mybutton = document.getElementById("btn-back-to-top");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  mybutton.addEventListener("click", backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  $('#open').on('click', function(){
+    $('.menu').toggleClass('is-active');
+  });
+
+
+  $('.jump').on('click', function(){
+    $('.menu').toggleClass('is-active');
+  });
+});
